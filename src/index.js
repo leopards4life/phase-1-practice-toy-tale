65557b13d992c0e76566aa55b844011752536d3d
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-01-17 15:50:31 -0700

    Added fetch toy function and rendered as cards

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  fetchToys();
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -12,4 +13,23 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+function fetchToys() {
+  fetch("http://localhost:3000/toys")
+  .then(res => res.json())
+  .then(json => renderToys(json))
+};
+
+  function renderToys(toys) {
+    const toyCollection = document.getElementById("toy-collection");
+    toys.forEach(toy => {
+      const card = document.createElement("div");
+      card.className = "card";
+      card.innerHTML = `<h2>${toy.name}</h2>
+      <img src="${toy.image}" class="toy-avatar">
+      <p>${toy.likes} Likes</p>
+      <button class="like-btn" id=${toy.id}> Like ❤️ </button>`
+      toyCollection.appendChild(card);
+    });
+  };
 });
